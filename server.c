8ae687fcb5ea3f7b28ade7afc7af42af54d27518
Szymon <32157216+Zawartas@users.noreply.github.com>
2018-06-27 20:46:30 +0200

    Add files via upload

--- server.c
+++ server.c
@@ -1,5 +1,7 @@
 #include "server.h"
 
+struct TOWAR tab_TOWAR[MAX_BUF];
+int database_size;
 
 int main(int argc, char *argv[])
 {	
@@ -42,7 +44,65 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
+/**
+ * Open database file
+ * and fill up tab_TOWAR table of TOWAR structures
+ * and fills up database size
+ */
+int open_database(int* database_size)
+{
+	FILE * fp;
+    	char * line = NULL;
+    	char * token = NULL;
+    	size_t len = 0;
+    	ssize_t read;
+    	int i = 0;
 
+    	fp = fopen(".baza", "r+");
+    	if (fp == NULL)
+        	exit(EXIT_FAILURE);
+        else
+        	printf("[+]Otworzono BAZE DANYCH\n");
+
+    	while ((read = getline(&line, &len, fp)) != -1) 
+    	{
+		if ((token = strtok(line, ",")) == NULL)
+            		exit(EXIT_FAILURE);
+
+    		sscanf(token, "%ld", &tab_TOWAR[i].kod_kreskowy);
+    		token = strtok (NULL, ",");
+    
+    		sscanf(token, "%s", tab_TOWAR[i].nazwa);
+    		token = strtok (NULL, ",");
+    
+    		sscanf(token, "%lg", &tab_TOWAR[i].cena);
+        i++;
+    	}
+	*database_size = i;
+	
+    	fclose(fp);
+    	if (line) free(line);
+    	
+    	return *database_size;
+}
+
+/**
+DO ZROBIENIA
+ */
+void save_database()
+{
+	FILE * fp;
+
+    fp = fopen(".baza", "wb");
+    for (int j = 0; j < database_size; j++)
+    {
+        if (strcmp(tab_TOWAR[j].nazwa, "do_skasowania") == 0) continue;
+        fprintf (fp, "%d,%s,%.2f\n", tab_TOWAR[j].kod_kreskowy, tab_TOWAR[j].nazwa, tab_TOWAR[j].cena);
+    }
+    fclose(fp);
+    
+    exit(EXIT_SUCCESS);
+}
 
 /**
  * Send file specified in filename over data connection, sending
@@ -84,9 +144,6 @@ void ftserve_retr(int sock_control, int sock_data, char* filename)
 		fclose(fd);
 	}
 }
-
-
-
 
 
 /**
@@ -205,6 +262,54 @@ int ftserve_pokaz_towar(int sock_data, int sock_control, long int szukane_ID)
 	return 0;	
 }
 
+
+/**
+ * Deleting product by a bar code
+ */
+int ftserve_skasuj_towar(int sock_data, int sock_control, long int szukane_ID)
+{
+	//int database_size;
+    	char data[MAXSIZE];
+    	int znaleziono = 0, i;
+    	i = open_database(database_size);
+
+        printf("[+]Poszukiwanie ID: %ld\n", szukane_ID);
+
+	memset(data, 0, MAXSIZE);
+    while (i) 
+    {   
+                if (tab_TOWAR[i].kod_kreskowy == szukane_ID)
+                {
+                    memset(data, 0, MAXSIZE);
+                    
+                    sprintf(data, "Skasowano produkt: %ld %s %lg\n", tab_TOWAR[i].kod_kreskowy, tab_TOWAR[i].nazwa, tab_TOWAR[i].cena);
+                    strcpy(tab_TOWAR[i].nazwa, "do_skasowania");
+                    printf("Znaleziono na serwerze produkt: %s\n", data);
+                    znaleziono = 1;
+                    break;
+                }
+                else
+                {   
+                    sprintf(data, "Nie znaleziono produktu o ID: %ld\n", tab_TOWAR[i].kod_kreskowy, tab_TOWAR[i].nazwa, tab_TOWAR[i].cena);
+                    strcpy(tab_TOWAR[i].nazwa, "do_skasowania");
+                    printf("Znaleziono na serwerze produkt: %s\n", data);
+                }
+                i--;
+    }
+	
+    if (line) free(line);
+
+	send_response(sock_control, 1); //starting
+
+		if (send(sock_data, data, strlen(data), 0) < 0) 
+        	{
+			perror("err");
+		}
+
+	send_response(sock_control, 226);	// send 226 NIE WIEM PO CO TO
+
+	return 0;	
+}
 
 
 /*___________________________________________________________________________________________________________*/
